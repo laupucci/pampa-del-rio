dd506e5dd839813eb6874efe6f6a8a820a136bfc
laupucci <laupuccinelli@gmail.com>
2021-01-29 19:01:51 -0300

    contact

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,13 +1,95 @@
 import React from "react";
+import { Formik, Field, Form } from "formik";
+import { useHistory } from "react-router-dom";
+import * as Yup from "yup";
 import { Link } from "react-router-dom";
 import { ContactContainer } from "./style";
 
 
 export default function Contact() {
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Required"),
+    email: Yup.string().email().required("Required"),
+    phone: Yup.number(),
+    description: Yup.string().required("Required"),
+  });
+  const history = useHistory();
+  
   return (
-    <div>
-      <ContactContainer>
-      </ContactContainer>
-    </div>
+    <Formik
+      initialValues={{
+        name: "",
+        email: "",
+        phone: "",
+        description: "",
+      }}
+      validationSchema={validationSchema}
+      onSubmit={async (values) => {
+      }}
+      >
+      {({ values, errors, touched }) => (
+          <ContactContainer>
+            <h2>Contactános!</h2>
+          <Form>
+            <label>Name</label>
+            <div className="inputConteiner">
+              <Field
+                className="input"
+                type="text"
+                name="name"
+                placeholder="Nombre..."
+              />
+              {touched.name && errors.name ? <div>{errors.name}</div> : null}
+            </div>
+            <div className="labelIn">
+                  <label>Email</label>
+                  <div className="inputConteiner">
+                    <Field
+                      name="email"
+                      type="email"
+                      className="input"
+                      placeholder="Mail..."
+                    />
+                    {touched.email && errors.email ? <div>Required</div> : null}
+                  </div>
+                </div>
+
+                <div className="labelIn">
+                  <label>Teléfono</label>
+                  <div className="inputConteiner">
+                    <Field
+                      name="phone"
+                      type="number"
+                      className="input"
+                      placeholder="Teléfono..."
+                    />
+                    {touched.phone && errors.phone ? <div>Required</div> : null}
+                  </div>
+                </div>
+
+            <label>Description</label>
+            <div className="textareaConteiner">
+              <Field
+                component="textarea"
+                type="text"
+                name="description"
+                placeholder="Mensaje..."
+                className="textarea"
+              />
+
+              {touched.description && errors.description ? (
+                <div>{errors.description}</div>
+              ) : null}
+            </div>
+           
+            <div className="">
+              <button className="" type="submit">
+               Contactar
+              </button>
+            </div>
+          </Form>
+        </ContactContainer>
+      )}
+    </Formik>
   );
-}
+};
--- src/components/contact/style.js
+++ src/components/contact/style.js
@@ -1,9 +1,14 @@
 import splash from "../../media/Splash.png";
 import styled from "styled-components";
 import { colors } from "../utils";
-const { black, whitish, primaryColor, primaryColorDarker } = colors;
+const { black, darkColor, whitish, primaryColor, primaryColorDarker } = colors;
 
 export const ContactContainer = styled.section`
-  color: ${whitish};
+  color: ${darkColor};
   height: 97vh;
+  margin-top: 1em;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center
   `
