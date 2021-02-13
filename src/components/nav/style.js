d1122f8f47385b8baeddd06d353b798878ab1da0
laupucci <laupuccinelli@gmail.com>
2021-02-12 22:11:10 -0300

    contact media

--- src/components/nav/style.js
+++ src/components/nav/style.js
@@ -90,12 +90,16 @@ export const Container = styled.header`
       min-height: 10vh
     }
   } 
+  .top{
+    width: 8%;
+    height: 6%;
+  }
   }
 
   @media (max-width: 400px) {
   .top{
-    width: 10%;
-    height: 6%;
+    width: 7.2%;
+    height: 4.2%;
   }
   }
 `;
