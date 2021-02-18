1227fe9ad3c172b2086885d4b6de771844665f29
Juanque <juan.castro.arancibia@gmail.com>
2021-02-18 00:12:44 -0300

    agrega la sidebar

--- src/components/nav/style.js
+++ src/components/nav/style.js
@@ -1,142 +1,118 @@
 import { colors } from "../utils";
 import styled from "styled-components";
-const { whitish } = colors;
+const { primaryColor, whitish } = colors;
 
 export const Container = styled.header`
-  position: fixed;
-  top: 0;
-  width: 100%;
-  font-family: "'Julius Sans One";
-  background-color: ${({ active }) => (active ? "rgba(0, 0, 0, 0.5)" : "none")};
-  height: 10vh;
   padding: 0 12vw;
+  position: fixed;
+  width: 100vw;
+  height: 10vh;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  z-index: 5;
+  background-color: ${({ active }) => (active ? "rgba(0, 0, 0, 0.5)" : "none")};
   transition: background 0.5s ease;
+  z-index: 5;
 
   .logo {
     font-size: 1.5rem;
     font-family: "Julius Sans One";
     color: ${whitish};
-    padding: 1em;
-  }
-  .menu {
-    display: flex;
-    flex-direction: "row";
-  }
-  .p2 {
-    color: ${whitish};
-    padding: 1em;
-    font-size: 16px;
-    font-family: "Sanchez";
-    &:hover {
-      background-color: rgba(163, 189, 49, 0.6);
-      border-radius: 7px;
-    }
-  }
-  a {
-    text-decoration: none;
-    cursor: pointer;
+    padding: 1rem;
   }
 
-  .top {
+  .menu_open,
+  .menu_close {
+    display: none;
+  }
+
+  .menu {
+    display: flex;
+
+    .menu_item {
+      color: ${whitish};
+      padding: 1rem;
+      cursor: pointer;
+      transition: 0.3s;
+
+      &:hover {
+        background-color: rgba(163, 189, 49, 0.6);
+        border-radius: 7px;
+      }
+    }
+  }
+
+  .top img {
     position: fixed;
     right: 1rem;
     bottom: 1rem;
-    width: 4rem;
-    height: 4rem;
-    border-radius: 50%;
-    border: 2px solid ${whitish};
-    background: none;
-    font-size: 3rem;
-    font-weight: bold;
-    display: ${({ active }) => (active ? "flex" : "none")};
-    justify-content: center;
-    align-items: center;
-    color: ${whitish};
+    width: 10vw;
+    max-width: 50px;
+    height: 10vw;
+    max-height: 50px;
+    display: ${({ active }) => (active ? "block" : "none")};
     cursor: pointer;
-    span {
-      position: relative;
-      top: 0.5rem;
-    }
   }
 
-  @media (max-width: 900px) {
-    width: 100%;
-    justify-content: space-between;
-
-    .logo {
-      font-size: 2vw;
-    }
-    .menu {
-      justify-content: flex-end;
-      align-items: center;
-    }
-
-    .p2 {
-      color: ${whitish};
-      font-size: 1.7vw;
-      width: 17vw;
-      //height: 4vh;
-      font-family: "Sanchez";
-      justify-content: flex-end;
-      align-items: center;
-      margin: 3%;
-      &:hover {
-        background-color: rgba(163, 189, 49, 0.6);
-        border-radius: 7px;
-        min-height: 7vh;
-      }
-    }
-    .top {
-      width: 6%;
-      height: 6%;
-    }
-  }
-  
-  @media screen and (max-width: 500px) {
-    width: 100%;
-    justify-content: space-between;
-    padding: 0;
-
-    .logo {
-      font-size: 4vw;
-      padding: 3px 3px 3px 13px;
-   
-    }
-    .menu {
-      justify-content: flex-end;
-      align-items: center;
-    }
-
-    .p2 {
-      color: ${whitish};
-      font-size: 2.6vw;
-      width: 26vw;
-      font-family: "Sanchez";
-      justify-content: flex-end;
-      align-items: center;
-      margin: 3%;
-      &:hover {
-        background-color: rgba(163, 189, 49, 0.6);
-        border-radius: 7px;
-        min-height: 7vh;
-      }
-    }
-    .top {
-      width: 8%;
-      height: 4.7%;
-    }
-  }
-  @media (max-width: 400px) {
+  @media screen and (max-width: 900px) {
     width: 100vw;
     padding: 0;
     justify-content: space-between;
-    .top {
-      width: 7.2%;
-      height: 4.2%;
+
+    .logo {
+      font-size: 1.5rem;
+    }
+
+    .menu_close.active {
+      display: block;
+      width: 1.5rem;
+      height: 1.5rem;
+      position: absolute;
+      top: 1rem;
+      right: 1.5rem;
+      z-index: 11;
+      cursor: pointer;
+    }
+    .menu_open.active {
+      display: block;
+      width: 1.7rem;
+      height: 1.7rem;
+      cursor: pointer;
+    }
+
+    .menu {
+      position: absolute;
+      padding-top: 2rem;
+      top: -100vh;
+      left: 0;
+      width: 100vw;
+      height: 100vh;
+      background: rgba(0, 0, 0, 0.8);
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+      transition: 0.3s ease-out;
+
+      .menu_item {
+        width: 100vw;
+        padding: 3rem;
+        &:hover {
+          background-color: ${primaryColor};
+          border-radius: 0%;
+        }
+        &:focus {
+          background-color: ${primaryColor};
+          border-radius: 0%;
+        }
+        p {
+          text-align: center;
+          font-size: 6vw;
+        }
+      }
+    }
+
+    .menu.active {
+      top: 0;
     }
   }
 `;
